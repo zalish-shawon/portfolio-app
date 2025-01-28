d8a19f526118588f4e6b0d2a26040531b1eeae9b
Md Zalish Mahmud <zalish.shawon@gmail.com>
2025-01-28 14:39:23 +0600

    routing and images added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
    <div>
-     <h1>Home page</h1>
+    <p>Home</p>
     </div>
   );
 }
--- src/components/footer/page.jsx
+++ src/components/footer/page.jsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
 import React from 'react';
+import styles from './footer.module.css'
 
 const Footer = () => {
     return (
         <div>
-      <div>©2023 Lamamia. All rights reserved.</div>
+            <div>©2023 Lamamia. All rights reserved.</div>
+            <div className={styles.social}>
+            <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Zalish Dev Facebook Account" />
+            <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Zalish Dev" />
+            <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Zalish Dev" />
+            <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Zalish Dev" />            
+         </div>
         </div>
     );
 };
