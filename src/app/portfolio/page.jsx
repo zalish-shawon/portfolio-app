4d8051cda1e6f0e15effd2f8d9ba3daaec16a1f8
Md Zalish Mahmud <zalish.shawon@gmail.com>
2025-01-29 16:17:40 +0600

    updated

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -13,9 +13,8 @@ const Portfolio = () => {
         <Link href="/portfolio/websites" className={styles.item}>
           <span className={styles.title}>Websites</span>
         </Link>
-        <Link href="/portfolio/application" className={styles.item}>
-          <span className={styles.title}>Application</span>
-        </Link>
+
+       
       </div>
     </div>
   );
