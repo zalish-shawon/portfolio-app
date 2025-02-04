e264af466c9d283ffb10310da69d460e6e370522
Md Zalish Mahmud <zalish.shawon@gmail.com>
2025-02-04 12:02:16 +0600

    upadted

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-const SingleBlog = () => {
-    return (
-        <div>
-            <h4>Blog details</h4>
+async function getData(id) {
+  const res = await fetch(`https://api.vercel.app/blog/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound()
+  }
+
+  return res.json();
+}
+
+
+export async function generateMetadata({ params }) {
+
+  const post = await getData(params.id)
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+}
+
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>{data.title}</h1>
+          <p className={styles.desc}>
+            {data.desc}
+          </p>
+          <div className={styles.author}>
+            <Image
+              src={"https://images.pexels.com/photos/5155309/pexels-photo-5155309.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>{data.username}</span>
+          </div>
         </div>
-    );
+        <div className={styles.imageContainer}>
+          <Image
+            src={"https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+         {data.content}
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default SingleBlog;
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -11,10 +11,10 @@ const Blog = async () => {
   return (
     <div className={styles.mainContainer}>
       {posts.map((item) => (
-        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+        <Link href={`/blog/${item.id}`} className={styles.container} key={item.id}>
           <div className={styles.imageContainer}>
             <Image
-              src={item.img}
+              src={"https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
               alt=""
               width={400}
               height={250}
