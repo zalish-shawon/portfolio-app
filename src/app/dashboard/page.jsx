b023b0d31157548baf8f9a060a562b9f7e2e63fd
Md Zalish Mahmud <zalish.shawon@gmail.com>
2025-02-09 12:21:36 +0600

    upgraded for dashboard api loader

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,30 +1,37 @@
 'use client'
 import React, { useEffect } from 'react';
 import styles from "./page.module.css";
+import useSWR from 'swr';
 
 const Dashboard = () => {
-     const [data, setData] = useState([]);
-  const [err, setErr] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        cache: "no-store",
-      });
+    
+    //      const [data, setData] = useState([]);
+    //   const [err, setErr] = useState(false);
+    //   const [isLoading, setIsLoading] = useState(false);
+    
+    //   useEffect(() => {
+        //     const getData = async () => {
+            //       setIsLoading(true);
+            //       const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+                //         cache: "no-store",
+                //       });
+                
+                //       if (!res.ok) {
+                    //         setErr(true);
+                    //       }
+                    
+                    //       const data = await res.json()
+                    
+                    //       setData(data);
+                    //       setIsLoading(false);
+                    //     };
+                    //     getData()
+                    //   }, []);
+                    
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
-      if (!res.ok) {
-        setErr(true);
-      }
-
-      const data = await res.json()
-
-      setData(data);
-      setIsLoading(false);
-    };
-    getData()
-  }, []);
 
     return (
         <div className={styles.container}> 
